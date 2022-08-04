25d5f5227ba5e33eb40c0e69b9dfcea16ec0ebc4
xiechenghua <chenghua.xie@ehang.com>
2022-08-04 14:37:01 +0800

    修正文件名错误的问题

--- src/helper.php
+++ src/helper.php
@@ -1,60 +1,127 @@
 <?php
 
 /**
- * 配置业务状态码
+ * 公共代码块
  *
  * @Author: 谢城华 xiechenghua01@163.com
- * @Time: 2022/8/3 16:22
+ * @Time: 2022/8/3 16:25
  */
 
-return [
-    '10000' => [
-        'msg' => '不存在的请求地址',
-        'httpStatus' => 500
-    ],
+/**
+ * 抛出http异常
+ *
+ * @param string $message 异常信息
+ * @param int $statusCode 异常状态码
+ */
+if (!function_exists('httpError'))
+{
+    function httpError(string $message, int $statusCode): void
+    {
+        abort($statusCode, $message);
+    }
+}
 
-    // 成功请求
-    '20000' => [
-        'msg' => '成功',
-        'httpStatus' => 200
-    ],
+/**
+ * http请求格式错误时抛出
+ *
+ * @param string $message 异常信息
+ */
+if (!function_exists('httpErrorBadRequest'))
+{
+    function httpErrorBadRequest(string $message = '错误请求'): void
+    {
+        httpError($message, 400);
+    }
+}
 
-    // 异常请求
-    '400' => [
-        'msg' => '错误请求',
-        'httpStatus' => 400
-    ],
-    '401' => [
-        'msg' => '未授权访问',
-        'httpStatus' => 401
-    ],
-    '403' => [
-        'msg' => '无权访问该资源',
-        'httpStatus' => 403
-    ],
-    '404' => [
-        'msg' => '资源不存在',
-        'httpStatus' => 404
-    ],
-    '405' => [
-        'msg' => '非法操作',
-        'httpStatus' => 405
-    ],
-    '422' => [
-        'msg' => '表单验证失败',
-        'httpStatus' => 422
-    ],
-    '429' => [
-        'msg' => '操作过于频繁，请稍后再试',
-        'httpStatus' => 429
-    ],
-    '500' => [
-        'msg' => '服务器错误',
-        'httpStatus' => 500
-    ],
-    '503' => [
-        'msg' => '资源未准备好',
-        'httpStatus' => 503
-    ],
-];
+/**
+ * 未授权访问资源，一般用于请求方没有提供有效令牌时抛出异常
+ *
+ * @param string $message 异常信息
+ */
+if (!function_exists('httpErrorUnauthorized'))
+{
+    function httpErrorUnauthorized(string $message = '未授权访问'): void
+    {
+        httpError($message, 401);
+    }
+}
+
+/**
+ * 非法访问资源，一般用于请求方访问没有权限的资源
+ *
+ * @param string $message 异常信息
+ */
+if (!function_exists('httpErrorForbidden'))
+{
+    function httpErrorForbidden(string $message = '无权访问该资源'): void
+    {
+        httpError($message, 403);
+    }
+}
+
+/**
+ * 资源不存在
+ *
+ * @param string $message 异常信息
+ */
+if (!function_exists('httpErrorNotFound'))
+{
+    function httpErrorNotFound(string $message = '资源不存在'): void
+    {
+        httpError($message, 404);
+    }
+}
+
+/**
+ * 请求方法不允许
+ *
+ * @param string $message 异常信息
+ */
+if (!function_exists('httpErrorMethodNotAllowed'))
+{
+    function httpErrorMethodNotAllowed(string $message = '非法操作'): void
+    {
+        httpError($message, 405);
+    }
+}
+
+/**
+ * 请求实体无法处理，一般用于表单验证失败
+ *
+ * @param string $message 异常信息
+ */
+if (!function_exists('httpErrorUnprocessableEntity'))
+{
+    function httpErrorUnprocessableEntity(string $message = '表单验证失败'): void
+    {
+        httpError($message, 422);
+    }
+}
+
+/**
+ * 服务器内部错误
+ *
+ * @param string $message 异常信息
+ */
+if (!function_exists('httpErrorInternal'))
+{
+    function httpErrorInternal(string $message = '服务器错误'): void
+    {
+        httpError($message, 500);
+    }
+}
+
+/**
+ * 资源暂时未准备好，要求请求方暂时不访问资源，如果可以，返回消息中可以包含可以正常访问资源的时间
+ *
+ * @param string $message 异常信息
+ */
+if (!function_exists('httpErrorNotReady'))
+{
+    function httpErrorNotReady(string $message = '资源未准备好'): void
+    {
+        httpError($message, 503);
+    }
+}
 
