cb229c545b0f0e8cf15f13e6f51ea8dd9c8b1a04
xiechenghua <chenghua.xie@ehang.com>
2022-08-04 15:32:34 +0800

    将数据转换类改成helper方法

--- src/helper.php
+++ src/helper.php
@@ -7,6 +7,31 @@
  * @Time: 2022/8/3 16:25
  */
 
+if (!function_exists('show'))
+{
+    function show(int $code = 0, array $data = [], string $msg = '', int $httpStatus = 0): \think\response\Json
+    {
+        //如果消息提示为空，并且业务状态码定义了，那么就显示默认定义的消息提示
+        if (empty($msg) && !empty(config("transformation.{$code}")))
+        {
+            $msg = config("transformation.{$code}.msg");
+        }
+
+        // http状态码
+        $httpStatus = empty($httpStatus) && !empty(config("transformation.{$code}.httpStatus"))
+            ? config("transformation.{$code}.httpStatus")
+            : 200;
+
+        $result = [
+            'code' => $code,
+            'message' => $msg,
+            'data' => $data
+        ];
+
+        return json($result, $httpStatus);
+    }
+}
+
 /**
  * 抛出http异常
  *
